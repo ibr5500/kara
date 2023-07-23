e851dea01b664abd68233ba544db95f8b76b30b8
unknown <ibr.s.ahmat@gmail.com>
2023-07-23 23:19:14 +0300

    Fix Login Page linter errors

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import {
-  Form, Button, Col, Row, Container, FloatingLabel,
+  Form, Button, Col, Row, Container, FloatingLabel, Modal,
 } from 'react-bootstrap';
 import { auth } from '../config/firebase-config';
 
@@ -13,41 +13,54 @@ type UserData = {
 }
 
 const Login: React.FC = (): JSX.Element => {
+  const navigate = useNavigate();
+  const [show, setShow] = useState(false);
+  const [showError, setShowError] = useState(false);
   const { register, handleSubmit, reset } = useForm<UserData>();
 
-  const navigate = useNavigate();
-
-  const onSubmit: SubmitHandler<UserData> = async (elem) => {
-    await signInWithEmailAndPassword(auth, elem.email, elem.password);
-    await sessionStorage.setItem('email', elem.email);
-    console.log('Admin logged in - successfully');
+  const handleClose = (): any => {
+    setShow(false);
+    setShowError(false);
     reset();
-    navigate('/');
   };
 
-  // const LogOut = async () => {
-  //   try {
-  //     await signOut(auth);
-  //     setTimeout(() => {
-  //       sessionStorage.clear();
-  //       console.log('Logged out - successfully');
-  //       navigate('/login');
-  //     }, 2500);
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // };
+  const handleShow = (): any => {
+    setShow(true);
+    setShowError(true);
+  };
+
+  const onSubmit: SubmitHandler<UserData> = async (elem): Promise<void> => {
+    try {
+      await signInWithEmailAndPassword(auth, elem.email, elem.password);
+      await sessionStorage.setItem('email', elem.email);
+      console.log('Admin logged in - successfully');
+      reset();
+      navigate('/');
+    } catch (err: any) {
+      handleShow();
+    }
+  };
 
   return (
-    <Container className="mt-5 d-flex justify-content-center">
-      <Form onSubmit={handleSubmit(onSubmit)}>
+    <Container className="mt-5 w-auto d-flex justify-content-center">
+      <Form className="w-50" onSubmit={handleSubmit(onSubmit)}>
         <Row className="mt-5 me-auto">
           <Col className="mt-5 me-auto">
             <FloatingLabel label="Admin&apos;s Email address" className="m-3">
-              <Form.Control required type="text" defaultValue="admin@gmail.com" {...register('email')} />
+              <Form.Control
+                required
+                type="text"
+                defaultValue="admin@gmail.com"
+                {...register('email')}
+              />
             </FloatingLabel>
             <FloatingLabel label="Admin&apos;s Password" className="m-3">
-              <Form.Control required type="password" placeholder="Password" {...register('password')} />
+              <Form.Control
+                required
+                type="password"
+                placeholder="Password"
+                {...register('password')}
+              />
             </FloatingLabel>
           </Col>
         </Row>
@@ -55,6 +68,25 @@ const Login: React.FC = (): JSX.Element => {
           Login
         </Button>
       </Form>
+      {showError && (
+        <Modal
+          className="popup"
+          show={show}
+          onHide={handleClose}
+          size="sm"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Body className="popup-message">Wrong password. Please try again.</Modal.Body>
+          <Button
+            className="popup-close"
+            variant="secondary"
+            onClick={handleClose}
+          >
+            Close
+          </Button>
+        </Modal>
+      )}
     </Container>
   );
 };
