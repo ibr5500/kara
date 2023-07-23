89207f8c2fbfa24ff1442d31a9f3c5693babbbfe
unknown <ibr.s.ahmat@gmail.com>
2023-07-23 23:39:12 +0300

    Fix Linter Errors

--- src/components/NewStudent.tsx
+++ src/components/NewStudent.tsx
@@ -9,49 +9,72 @@ import { db } from '../config/firebase-config';
 
 type FormValues = {
   name: string;
-  description: string;
+  email: string;
+  age: number;
   creation_at: string;
+  image: string;
 };
 
-const NewCourse: React.FC = (): JSX.Element => {
+const NewStudent: React.FC = (): JSX.Element => {
   const navigate = useNavigate();
   const { register, handleSubmit, reset } = useForm<FormValues>();
 
-  const coursesList = collection(db, 'courses');
+  const studentsList = collection(db, 'students');
 
   const onSubmit: SubmitHandler<FormValues> = async (data): Promise<void> => {
-    await addDoc(coursesList, data);
+    await addDoc(studentsList, data);
     reset();
-    navigate('/courses');
+    navigate('/students');
   };
 
   return (
     <Container className="mt-2 w-auto d-flex justify-content-center">
       <Form onSubmit={handleSubmit(onSubmit)} className="d-flex flex-column w-50 gap-3">
-        <h2>Create New Course</h2>
+        <h2>Create New Student</h2>
         <Col>
-          <FloatingLabel label="Course's Name">
+          <FloatingLabel label="Student's Name">
             <Form.Control
               required
               type="text"
-              placeholder="Course's Name"
+              placeholder="Full Name"
               {...register('name')}
             />
           </FloatingLabel>
         </Col>
         <Col>
-          <FloatingLabel label="Course's Description">
+          <FloatingLabel label="Student's Email">
             <Form.Control
               required
-              as="textarea"
-              style={{ height: '100px' }}
-              placeholder="Course's Description"
-              {...register('description')}
+              type="email"
+              placeholder="email@example.com"
+              {...register('email')}
             />
           </FloatingLabel>
         </Col>
         <Col>
-          <FloatingLabel label="Creation Date">
+          <FloatingLabel label="Student's Age">
+            <Form.Control
+              required
+              type="number"
+              min={18}
+              max={100}
+              placeholder="Age"
+              {...register('age')}
+            />
+          </FloatingLabel>
+        </Col>
+        <Col>
+          <FloatingLabel label="Student's Image URL">
+            <Form.Control
+              required
+              type="url"
+              placeholder="Image's URL"
+              {...register('image')}
+            />
+          </FloatingLabel>
+        </Col>
+        <Col>
+          <FloatingLabel label="Join Date">
             <Form.Control
               required
               type="date"
@@ -59,10 +82,10 @@ const NewCourse: React.FC = (): JSX.Element => {
             />
           </FloatingLabel>
         </Col>
-        <Button type="submit" className="w-25 mx-5" variant="success">Add Course</Button>
+        <Button type="submit" className="w-25 mx-5" variant="success">Add Student</Button>
       </Form>
     </Container>
   );
 };
 
-export default NewCourse;
+export default NewStudent;
